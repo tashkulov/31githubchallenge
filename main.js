d9437b443886700986081ed2a1f39a16bbf1a13b
tashkulov <nurislamtashkulov168@gmail.com>
2023-11-22 14:56:23 +0600

    start

---
+++ main.js
@@ -0,0 +1,23 @@
+
+
+// console.log(5,34,334,34,)
+// console.error(4)
+// console.warn(3)
+// console.group("Num",55,345,4534,345,3,534,545,45,345,43,434)
+
+
+
+// console.log(a)
+// var a = 1
+// console.log(a)
+
+// let b = 2
+// const c= 6
+// console.log(b)
+
+
+// b=10
+// console.log(b)
+
+
+const a= 5+10

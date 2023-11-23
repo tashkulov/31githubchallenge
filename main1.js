2d75e7d3c8fee41324014e143f5b945dd313603a
tashkulov <nurislamtashkulov168@gmail.com>
2023-11-23 10:49:18 +0600

    this

---
+++ main1.js
@@ -0,0 +1,112 @@
+// const a= 5
+// const b= 10
+
+// if (a===b) {
+//     console.log('yes')
+// }else if(a===9){
+//     console.log(a,'9')
+// }else{
+//     console.log('NO')
+// }
+
+//&&- OR
+// console.log(5 &&5 )
+// console.log(5 &&0 )
+
+//  swich case
+// const a=7
+// switch (a) {
+//     case 5:{
+//         console.log(a)
+//     }
+//     break;
+
+//     case 6:{
+//         console.log("No")
+//     }
+//     break;
+
+//     case 1:{
+//         console.log('No')
+//     }
+        
+//         break;
+
+//     default:{
+//         console.log('default')
+//     }
+//         break;
+// }
+
+
+// //тернарный оператор
+// const a = 5
+// const b=20
+
+// a>b 
+// ? console.log('a is bigger than b')
+// : console.log('b is bigger then a')
+
+// const text=`${a} plus ${b} `
+
+//promt,alert ,confirm
+
+//promt - input
+//alert-окно уведомление 
+//confirm-окно для подтверждения
+
+// const a = prompt('enter first num: ')
+// const b = prompt('enter second num: ')
+
+// const symb= prompt(`Choose one of them 
+// 1)+
+// 2)-
+// 3)*
+// 4)/
+// `)
+
+// if(typeof +a!=='number ' 
+// || typeof +b!=='number'){
+//     alert('first and second values must be a numbers')
+// }
+// else if(a && b && symb){
+//     const numA=+a
+//     const numB=+b
+
+   
+
+//     switch (symb) {
+
+//         case '1' :
+//         case '+':{
+//             alert(numA+numB)
+//         }
+//         break;
+//         case '2' :
+//         case '-':{
+//             alert(numA-numB)
+//         }
+//         break;
+//         case '3' :
+//         case '*':{
+//             alert(numA*numB)
+//         }
+//         break;
+//         case '4' :
+//         case '/':{
+//             alert(numA/numB)
+//         }
+//         break;
+
+//         default:{
+//             alert('Could not find this symb')
+//         }
+//     }
+    
+// }else{
+//     alert('Enter correct data')
+// }
+
+
+//lesson3
+

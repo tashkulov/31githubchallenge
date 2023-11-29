a28fbae068bb947f9d2b4390c64e2e20eb0ea1e8
tashkulov <nurislamtashkulov168@gmail.com>
2023-11-29 22:18:25 +0600

    added

---
+++ DOM_JS/main.js
@@ -0,0 +1,71 @@
+
+// const tagBoxes = document.getElementsByTagName('div')
+// const classBoxes = document.getElementsByClassName('box')
+// const nameBoxes = document.getElementsByName('box')
+// const idBox = document.getElementById('lastBox')
+
+// const tagBoxesQuery = doctument.querySelectorAll('.box')
+// const idBoxQuery = document.querySelector('.box')
+
+// idBox.setAttribute('name', 'lastBox')
+// const nameIdBox = idBox.getAttribute('name')
+// console.log({ nameIdBox })
+// idBox.removeAttribute('id')
+// console.log(idBox.hasAttribute('id'))
+
+// const button = document.createElement('button')
+
+// button.style.color = 'red'
+// button.style.cssText = `
+//  border: 2px solid red;
+//  background: transparent;
+// `
+
+// button.innerText = 'append'
+// document.body.append(button) // добавляет в конец (внутрь body)
+// button.innerText = 'prepend'
+// document.body.prepend(button) // добавляет в начало (внутрь body)
+// button.innerText = 'before'
+// document.body.before(button) // добавляет до body (не в body)
+// button.innerText = 'after'
+// document.body.after(button) // добавляет после body (не в body)
+
+
+// button.classList.add('btn') // добавил класс кнопке
+// button.classList.remove('btn') // удалил этот класс у кнопки
+// button.classList.contains('btn') // проверяет есть ли такой класс true/false
+// button.classList.toggle('btn') // если есть - удаляет, если нет - добавляет
+// button.classList.toggle('btn')
+// button.classList.toggle('btn')
+
+
+// {/* <div>
+//   <ul>
+//     <li>1</li>
+//     <li>2</li>
+//     <li>3</li>
+//   </ul>
+// </div> */}
+
+
+// const div = document.createElement('div')
+// const ul = document.createElement('ul')
+
+// for(let i = 0; i < 3; i++) {
+//     const li = document.createElement('li')
+//     li.innerText = i + 1
+//     ul.append(li)
+// }
+
+// div.append(ul)
+
+// document.body.append(div)
+
+
+// if(Math.random() > 0.5) {
+//     document.body.style.background = 'red'
+// } else {
+//     document.body.style.background = 'green'
+// }
+
+

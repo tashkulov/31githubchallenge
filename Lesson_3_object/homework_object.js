54fd0dc40b36646138a8f62467728b35430acb8b
tashkulov <nurislamtashkulov168@gmail.com>
2023-12-06 22:42:18 +0600

    added

---
+++ Lesson_3_object/homework_object.js
@@ -0,0 +1,45 @@
+// //1
+// let person= {name:'Nuris',age:16,city:'Bishkek'}
+// console.log(person)
+
+// //2
+// person.name='Nuritto buritto'
+// console.log(person)
+
+// //3
+// const numbers =[1,2,3,4,5]
+// console.log(numbers.length)
+
+// //4
+// const animals=[]
+// animals[0]='lion'
+// animals[1]='tiger'
+// animals[2]='elephant'
+// animals[3]='deer'
+// animals[4]='fly'
+// console.log(animals)
+
+// //5
+// let person1= {name:'Nuris',age:16,city:'Bishkek'}
+// let person2= {name:'Puris',age:17,city:'Bishkek'}
+// let person3= {name:'Turis',age:19,city:'Bishkek'}
+// const people=[person1,person2,person3]
+// console.log(people)
+
+// //6
+// const data =[1,'pudge',person]
+// console.log(data)
+
+// //7
+// const fruits=['apple','banana','orange']
+// console.log(fruits[2])
+
+// //8
+// const book={title:'One piece',
+//                  author:'Eiichiro Oda',
+//                  year:1997
+//                 }
+// console.log(book)
+// console.log(book.author)
+
+
---
+++ Lesson_3_object/main.js
@@ -0,0 +1,30 @@
+// let a =5
+// let b= a
+// a=10
+// console.log(b)
+
+                                              //object-ссылочные 
+
+
+// let obj={
+//     name:'aidar',
+//     age: 21
+// }
+// let obj2=obj
+// obj.age=30
+// console.log(obj2)
+
+//присвоение 
+
+
+// const obj = {
+//     name:'nuris',
+//     age:20
+// }
+// obj.name='ne nuris'
+// obj['age']=30
+// obj.language='js'
+// obj['country']='Kyrgyzstan' 
+// delete obj.name
+// console.log(obj)
+

1f0be2b6cb5df49cce9f21365221569a36297480
tashkulov <nurislamtashkulov168@gmail.com>
2023-12-15 14:48:20 +0600

    added

---
+++ Portal,Children/HomeworkChildren.jsx
@@ -0,0 +1,12 @@
+// import React from 'react';
+
+// const HomeworkChildren = (props) => {
+//   return (        
+//     <div style={{alignItems:'center'}}  >
+//       <button style={{ backgroundColor: 'red',width:'400px', height:'300px'}}>{props.children}</button>
+//     </div>                        
+//   );
+// }
+
+
+// export default HomeworkChildren;
---
+++ Portal,Children/PortalHomeworks.jsx
@@ -0,0 +1,17 @@
+  // import React from 'react';
+  // import { createPortal } from 'react-dom';
+
+
+  // const PortalHomeworks = (props) => {
+  //   const {
+  //     children,
+  //     container =document.body
+  //   }=props
+  //   return createPortal(children,container)
+  // };
+
+  // export default PortalHomeworks;
+
+
+
+  
